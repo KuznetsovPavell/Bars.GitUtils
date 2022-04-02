dd33a6c10e8c2995b14ca04013aeb3e2ac852d4e
pavlo.kuznetsov <pavlo.kuznetsov@unity-bars.com>
2022-04-02 15:01:45 +0300

    Release 02.04.22

--- RepoHandler.cs
+++ RepoHandler.cs
@@ -1,20 +1,20 @@
 ﻿using LibGit2Sharp;
 using LibGit2Sharp.Handlers;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Text.RegularExpressions;
+
+// Функционал работы с репозиторием.
 
 namespace Bars.GitUtils
 {
+#nullable disable
     public class RepoHandler : IDisposable
     {
         private readonly Repository _repository;
-        private readonly string     _repoUser;
-        private readonly string     _repoPass;
+        private readonly string _repoUser;
+        private readonly string _repoPass;
 
         // User information to create a merge commit        
-        private readonly Signature signature = new Signature(new Identity("MERGE_USER_NAME", "MERGE_USER_EMAIL"), DateTimeOffset.Now);
+        private readonly Signature signature = new(new Identity("MERGE_USER_NAME", "MERGE_USER_EMAIL"), DateTimeOffset.Now);
         public Branch CurrentBranch { private set; get; }
         public RepoHandler(string repoPath, string repoUser, string repoPass)
         {
@@ -22,14 +22,18 @@ namespace Bars.GitUtils
             _repoUser = repoUser;
             _repoPass = repoPass;
         }
+
+        // ---=== Method ===--- //
         public void CheckoutBranch(string branch)
         {
             CurrentBranch = Commands.Checkout(_repository, _repository.Branches[branch]);
         }
+
+        // ---=== Method ===--- //
         public void PullBranch()
         {
             // Credential information to fetch
-            PullOptions options = new PullOptions
+            PullOptions options = new()
             {
                 FetchOptions = new FetchOptions
                 {
@@ -46,13 +50,16 @@ namespace Bars.GitUtils
             try
             {
                 Commands.Pull(_repository, signature, options);
-            }catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine("Server is not available\n\n");
                 Console.WriteLine(ex.ToString());
+                Console.WriteLine("----------Bars.GitUtils.RepoHandler.cs.PullBranch()----------");
             }
         }
-        
+
+        // ---=== Method ===--- //
         public MatchCollection DiffBetweenTagsSQL(string tagFrom, string tagTo)
         {
             Tree tagto = _repository.Lookup<Commit>(tagTo).Tree;      //tagTo
@@ -63,6 +70,7 @@ namespace Bars.GitUtils
             return outValue;
         }
 
+        // ---=== Method ===--- //
         public List<Tuple<string, string, ChangeKind>> DiffBetweenTagsWEB(string tagOld, string tagNew)
         {
             Tree tagnew = _repository.Lookup<Commit>(tagNew).Tree;  //tagTo
@@ -79,6 +87,27 @@ namespace Bars.GitUtils
             }
             return listOut;
         }
+
+        // ---=== Method ===--- //
+        public List<Tuple<string, string, ChangeKind>> DiffBetweenTagsPSQL(string tagFrom, string tagTo)
+        {
+            Tree tagto = _repository.Lookup<Commit>(tagTo).Tree;      //tagTo
+            Tree tagfrom = _repository.Lookup<Commit>(tagFrom).Tree;  //tagFrom
+
+            var changesFiles = _repository.Diff.Compare<Patch>(tagfrom, tagto, new List<string>() { "*" });
+            //            MatchCollection outValue = Regex.Matches(changesFiles.Patch, @"^\+@..*", RegexOptions.Multiline);
+            var listOut = new List<Tuple<string, string, ChangeKind>>();
+
+            foreach (var item in changesFiles)
+            {
+                listOut.Add(Tuple.Create(item.Path.Replace('/', Path.DirectorySeparatorChar).ToLower()
+                                   , item.OldPath.Replace('/', Path.DirectorySeparatorChar).ToLower()
+                                   , item.Status));
+            }
+            return listOut;
+        }
+
+        // ---=== Method ===--- //
         // Add
         public void AddFileToRepo(string fileToAdd)
         {
@@ -86,6 +115,7 @@ namespace Bars.GitUtils
             _repository.Index.Write();
         }
 
+        // ---=== Method ===--- //
         // Commit
         public void CommitRepoChanges(string message)
         {
@@ -93,9 +123,36 @@ namespace Bars.GitUtils
         }
 
         // Erase Object Repository
-        public void Dispose()
+        private bool disposed = false;
+
+        // ---=== Method ===--- //
+        protected virtual void Dispose(bool disposing)
         {
-            _repository.Dispose();
+            disposed = !disposed || disposed;
+        }
+
+        public void Dispose() // Implement IDisposable
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~RepoHandler() // the finalizer
+        {
+            Dispose(false);
+        }
+
+        // ---=== Method ===--- //
+        internal static string MakeColumn(string valJson, int width)
+        {
+            if (valJson == null) valJson = "";
+            if (valJson.Length > width)
+                valJson = valJson[..width];
+            return valJson.PadRight(width);
+        }
+
+        static void Main(string[] args)
+        {
         }
     }
 
